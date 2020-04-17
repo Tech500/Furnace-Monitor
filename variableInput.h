c24097e9a2444cf3c291d59803070323ac925b6f
William Lucid <11734452+Tech500@users.noreply.github.com>
2020-04-17 07:11:09 -0400

    Add files via upload

---
+++ variableInput.h
@@ -0,0 +1,30 @@
+//
+//   "Rain Gauge Two.ino" and  
+//   variableInput.h library
+//   William M. Lucid   10/06/2019   @ 15:02 EDT    
+// 
+
+// Replace with your network details  
+//const char * host  = "esp32";
+
+// Replace with your network details
+const char * ssid = "Oscar-2";
+const char * password = "sissy4357";
+
+
+//publicIP accessiable over Internet with Port Forwarding; know the risks!!!
+//WAN IP Address.  Or use LAN IP Address --same as server ip; no Internet access. 
+//#define publicIP  "73.102.122.239"  //Part of href link for "GET" requests
+//String LISTEN_PORT = "8030"; //Part of href link for "GET" requests
+
+
+
+//Server settings
+#define ip {172,16,0,7}
+#define subnet {255,255,255,0}
+#define gateway {172,16,0,1}
+#define dns {172,16,0,1}
+
+//FTP Credentials
+const char * ftpUser = "techno";
+const char * ftpPassword = "qrzab9nq";
